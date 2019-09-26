306b4cff5a8127d7247a886da5fb7f831254097c
suryafakhriy <55834014+suryafakhriy@users.noreply.github.com>
2019-09-26 21:34:18 +0700

    Add files via upload

---
+++ IF-01-10118028-Latihan9-Teks/src/pkgif/pkg01/pkg10118028/latihan9/teks/IF0110118028Latihan9Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif.pkg01.pkg10118028.latihan9.teks;
+
+/**
+ *
+ * @author User
+ * NAMA      : Surya Fakhriy Hasbi
+   KELAS     : IF-01 2018
+   NIM       : 10118028
+   DESKRIPSI : Teks
+ */
+public class IF0110118028Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char : " + tab + "berisi " + c );
+    }
+    
+}
